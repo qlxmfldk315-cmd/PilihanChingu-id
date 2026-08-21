928f197b1895edee505571f758b592b86a06d879
pilihanchingu-id <pilihanchingu.id@gmail.com>
2026-08-21 09:41:50 +0700

    remove devtools from production

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,16 +1,10 @@
 import * as React from 'react'
 import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export const Route = createRootRoute({
   component: RootComponent,
 })
 
 function RootComponent() {
-  return (
-    <>
-      <Outlet />
-      <TanStackRouterDevtools position="bottom-right" />
-    </>
-  )
+  return <Outlet />
 }
